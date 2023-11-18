0f602622fd9138473f5c4069a38625b4064d1c97
sharmas1ddharth <ssiddharth408@gmail.com>
2023-11-18 16:04:42 +0530

    added find_size_data_types

--- sum_of_two_numbers.cpp
+++ sum_of_two_numbers.cpp
@@ -1,3 +1,4 @@
+/* Write a program in C++ to print the sum of two numbers.*/
 #include <iostream>
 
 using namespace std;
--- welcome_text.cpp
+++ welcome_text.cpp
@@ -1,3 +1,7 @@
+/*
+Write a program in C++ to print welcome text on a separate line.
+*/
+
 #include <iostream>
 using namespace std;
 
