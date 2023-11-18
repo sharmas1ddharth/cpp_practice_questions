8cfc6902831c82a1c16e3d20ee36205fae6c0e6e
sharmas1ddharth <ssiddharth408@gmail.com>
2023-11-18 15:54:17 +0530

    added sum_of_two_numbers

---
+++ sum_of_two_numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int first_number = 10;
+    int second_number = 20;
+    int third_number = first_number + second_number;
+    std::cout << third_number << std::endl;
+    return 0;
+}
